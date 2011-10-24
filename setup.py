652890b69da99c7e29f291ac3da428bdd6bcf80a
Duncan Hanson <dhanson@jpl.nasa.gov>
2011-10-24 16:51:28 -0700

    remove unnecessary healpix module.

--- setup.py
+++ setup.py
@@ -10,11 +10,6 @@ import glob
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('qcinv',parent_package,top_path)
-    config.add_extension('_healpix', ['qcinv/_healpix.pyf', 'qcinv/_healpix.f90'],
-                         include_dirs=[healpix_incdir],
-                         library_dirs=[healpix_libdir, cfitsio_libdir],
-                         libraries=['healpix', 'cfitsio', 'gomp'],
-                         extra_compile_args=['-fopenmp'])
     return config
 
 if __name__ == "__main__":
